2b450d69fee14fd93679947fc313348df17db36f
kira1311 <git@github.com:kira1311/site-game.git>
2025-11-20 16:16:30 +0300

    Домашка переменные

---
+++ index.js
@@ -0,0 +1,46 @@
+let a = 10
+alert (a)
+
+a = 20
+alert(a)
+
+let iPhone = 2007
+alert(iPhone)
+
+let JSname = "Брендан Эйх"
+alert(JSname)
+
+let b = 10
+let c = 2
+
+alert(c+b)
+alert(b-c)
+alert(b*c)
+alert(b/c)
+
+let result = 2**5
+alert(result)
+
+let A = 9
+let B = 2
+
+alert(A%B)
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age = prompy('Сколько вам лет?')
+alert(age)
+
+let user = { name: 'JaneDou', 
+    age: 20, 
+    isAdmin: True };
+
+let userName = prompt('Ваше имя:')
+alert(`Привет, ${userName}!`)
